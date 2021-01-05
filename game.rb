597de110ab105659b8cd290727751642f67b9dea
rameshkumarvrs <rameshkumarvrs@gmail.com>
2021-01-05 15:53:37 +0530

    clean up the code

--- game.rb
+++ game.rb
@@ -15,11 +15,34 @@ class Hangman
 		]	
 	end
 
+	def word_size
+		word_length = ""
+		@game_name.first.size.times do
+			 word_length += "_ "
+		end
+    puts word_length
+  end
+
+  def make_guess
+  	puts "please Enter the letter"
+  	guess = gets.chomp
+  	good_guess = @game_name.first.include? guess.to_s
+  	if good_guess
+  		puts "good guess"
+  	else
+  	  puts "Sorry You went wrong guess"	
+  	end
+	end
+
+	def game_clues
+		puts "Your clue for the game name is #{@game_name.last}...."
+		puts "Clue for the game name size is #{@game_name.first.size} long"
+	end
+
 	def begin
-		puts "Your clue for the game  #{@game_name.last}...."
-		puts "Clue for the game name size is #{@game_name.first.size}"
-		guess = gets.chomp
-		puts "please Enter the letter"
+		game_clues
+    word_size
+		make_guess
 	end
 
 end	
